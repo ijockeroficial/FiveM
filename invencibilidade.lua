697d21b51a0f21fd0148a3ac0a86dea44eebe3ff
Rhógeris Lisboa <47046864+ijockeroficial@users.noreply.github.com>
2021-07-22 16:35:32 -0300

    Update invencibilidade.lua

--- invencibilidade.lua
+++ invencibilidade.lua
@@ -1,11 +1,11 @@
---Criado por iJocker YT https://youtube.com/ijockeroficiall
+--Comando de invencibilidade criado por iJocker YT https://youtube.com/ijockeroficiall
 --Baixado em https://github.com/ijockeroficial/FiveM
-
---COMANDO ADICIONADO NO LADO SERVIDOR (SERVER.LUA)
---COMANDO DE INVENSIBILIDADE
 RegisterCommand('invii',function(source,args,rawCommand)
 	local user_id = vRP.getUserId(source)
    	if vRP.hasPermission(user_id,"invencibilidade.permissao") then
 		SetPlayerInvincible(source, true)
 	end
 end)
+--FIM
+
+--Obs: COMANDO ADICIONADO NO LADO SERVIDOR (SERVER.LUA)
